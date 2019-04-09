c395ec974304a3a34454645ad3dea3451b993b84
Orange <787374484@qq.com>
2019-04-09 12:18:55 +0800

    Create Music_comment.java

---
+++ src/domain/Music_comment.java
@@ -0,0 +1,67 @@
+package domain;
+
+public class Music_comment {
+    private int id;
+    private String content;
+    private int music_id;
+    private int likes;
+    private String date;
+    private int user_id;
+
+    public Music_comment(int id, String content, int music_id, int likes, String date, int user_id) {
+        this.id = id;
+        this.content = content;
+        this.music_id = music_id;
+        this.likes = likes;
+        this.date = date;
+        this.user_id = user_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getMusic_id() {
+        return music_id;
+    }
+
+    public void setMusic_id(int music_id) {
+        this.music_id = music_id;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
