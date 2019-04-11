295d3e96c420da8125cf4ca0fc84c58ada22bd9f
Orange <787374484@qq.com>
2019-04-11 21:10:01 +0800

    Update User.java

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,4 +1,15 @@
 package domain;
 
 public class User {
+    private int id;
+    private String name;
+    private String nickname;
+    private String password;
+    private int gender;
+    private String email;
+    private String address;
+    private String signatrue;
+    private String image_url;
+    private String introduction;
+
 }
