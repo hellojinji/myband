1720c11930ea7b03e6e342dcb31e6939747e4719
huangzong1998 <916112430@qq.com>
2019-04-12 15:51:23 +0800

    添加getter方法
    还差setter方法，谁有空写一下

--- src/domain/User.java
+++ src/domain/User.java
@@ -12,4 +12,43 @@ public class User {
     private String image_url;
     private String introduction;
 
+    public int getId() {
+    	return this.id;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    
+    public String getNickName() {
+    	return this.nickname;
+    }
+    
+    public String getPassword() {
+    	return this.password;
+    }
+    
+    public int getGender() {
+    	return this.gender;
+    }
+    
+    public String getEmail() {
+    	return this.email;
+    }
+    
+    public String getAddress() {
+    	return this.address;
+    }
+    
+    public String getSignature() {
+    	return this.signatrue;
+    }
+    
+    public String getImageUrl() {
+    	return this.image_url;
+    }
+    
+    public String getIntroduction() {
+    	return this.introduction;
+    }
 }
