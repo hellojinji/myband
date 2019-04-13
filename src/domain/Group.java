f89f2a9c3abacb9e231e6be607f1013328cd8e6f
huangzong1998 <916112430@qq.com>
2019-04-13 12:45:00 +0800

    属性

--- src/domain/Group.java
+++ src/domain/Group.java
@@ -1,4 +1,40 @@
 package domain;
 
 public class Group {
+	private int id;
+	private String name;
+	private String introduction;
+	private int num;//成员数
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setIntroduction(String introduction) {
+		this.introduction=introduction;
+	}
+	
+	public String getIntroduction() {
+		return this.introduction;
+	}
+	
+	public void setNum(int num) {
+		this.num=num;
+	}
+	
+	public int getNum() {
+		return this.num;
+	}
 }
